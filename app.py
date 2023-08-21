1ba2f6b0f0bcc51cee9cb204917d08eae8d60bf9
davidchndra <david19ti@mahasiswa.pcr.ac.id>
2023-08-21 16:37:33 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 from datetime import datetime
 
-connection = "mongodb+srv://davidchndra:Davidmongodb@cluster0.wz8fbxs.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(connection)
-db = client.personalDiary
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
@@ -39,12 +47,15 @@ def save_diary():
     extension = profile.filename.split('.')[-1]
     profilename = f'static/profile-{mytime}.{extension}'
     profile.save(profilename)
-        
+    
+    time = today.strftime("%Y-%m-%d")
     doc = {
         "file": filename,
         "profile" : profilename,
         "title": title_receive, 
-        "content": content_receive
+        "content": content_receive,
+        "time" : time
+        
      }
     db.diary.insert_one(doc)
     return jsonify({"message": "Data was saved!"})
